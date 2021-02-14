69ed399f73f98e5b5143766e65cc45f07cb669a4
Jean Fonseca <fonsecajean42@gmail.com>
2021-02-14 20:42:16 -0300

    Configurando twig para layout

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function home()
+    {
+        return $this->render('base.html.twig');
+    }
+}
