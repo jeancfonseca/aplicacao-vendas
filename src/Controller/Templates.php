34669b84f580b50504689ed5cc18b71b31832ed7
Jean Fonseca <fonsecajean42@gmail.com>
2021-02-15 17:30:15 -0300

    listando vendedores

--- src/Controller/Templates.php
+++ src/Controller/Templates.php
@@ -2,11 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\VendedorRepository;
+use App\Services\Vendedor\VendedorService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Templates extends AbstractController
 {
+    private $entityManager;
+    private $vendedorRepository;
+
+    public function __construct (EntityManagerInterface $entityManager, VendedorRepository $vendedorRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->vendedorRepository = $vendedorRepository;
+    }
+
     /**
      * @Route("/", methods={"GET"})
      */
@@ -29,5 +41,17 @@ class Templates extends AbstractController
     public function cadastrarVenda()
     {
         return $this->render('/Venda/cadastrar_venda.html.twig');
+    }
+
+
+    /**
+     * @Route("/listar/vendedores", methods={"GET"})
+     */
+    public function clistarVendedor()
+    {
+        $vendedorService = new VendedorService($this->entityManager, $this->vendedorRepository);
+        $vendedores = $vendedorService->buscarVendedores();
+
+        return $this->render('/Vendedor/listar_vendedores.html.twig', ["vendedores" => $vendedores]);
     }
 }
