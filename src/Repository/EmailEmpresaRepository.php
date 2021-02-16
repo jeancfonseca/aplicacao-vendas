7295f4696c796d085f953c0f6ddb5582355d3ccb
Jean Fonseca <fonsecajean42@gmail.com>
2021-02-16 11:34:12 -0300

    Tabela email empresa e migration

---
+++ src/Repository/EmailEmpresaRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\EmailEmpresa;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method EmailEmpresa|null find($id, $lockMode = null, $lockVersion = null)
+ * @method EmailEmpresa|null findOneBy(array $criteria, array $orderBy = null)
+ * @method EmailEmpresa[]    findAll()
+ * @method EmailEmpresa[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EmailEmpresaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, EmailEmpresa::class);
+    }
+}
